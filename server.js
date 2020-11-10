8634211ba364a4b399a1c103739d9d2cdcd34a68
Pashtet <web.shorohov@yandex.ru>
2020-11-10 18:58:55 +0200

    first

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fs       = require('fs');
+const http     = require('http');
+const url      = require('url');
+const mime     = require('mime-types');
+const punycode = require('punycode');
+
+
+http.createServer((req, res) => {
+    
+}).listen('1212', '0.0.0.0');
